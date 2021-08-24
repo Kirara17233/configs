c5920e5767cfde14e71b56714980a7b38c8b8418
Kirara17233 <85111701+Kirara17233@users.noreply.github.com>
2021-08-24 05:05:05 +0300

    Delete step3.sh

--- step3.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-user=#user
-
-curl -o /etc/ssh/.ssh/id_rsa "https://raw.githubusercontent.com/Kirara17233/rsa/main/id_rsa?token=$1"
-ln -s /etc/ssh/.ssh/id_rsa /etc/skel/.ssh/id_rsa
-ln -s /etc/ssh/.ssh/id_rsa /home/$user/.ssh/id_rsa
-rm /step3.sh
