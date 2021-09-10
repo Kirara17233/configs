d26190d2e56a33b5ca8bd3fe7f0bc46bbda7c840
Kirara17233 <85111701+Kirara17233@users.noreply.github.com>
2021-09-10 15:34:54 +0300

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -54,7 +54,7 @@ main = do
       startupHook        = do
                             spawnOnce "/usr/bin/numlockx on"
                             spawnOnce "picom"
-                            spawn "xwallpaper --daemon --zoom /etc/config/wallpapers/$[$RANDOM%`ls -l /etc/config/wallpapers | grep \"^-\" | wc -l`].jpg"
+                            spawn "xwallpaper --daemon --zoom /etc/config/wallpapers/$[$RANDOM%`ls -l /etc/config/wallpapers | grep '^-' | wc -l`].jpg"
                             spawnOnce "xsetroot -cursor_name left_ptr"
                             spawnOnce "xfce4-panel"
                             spawnOnce "jetbrains-toolbox --minimize"
