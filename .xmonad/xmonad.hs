31936ac05d4fdb074e9313383708fb33b1e00161
Kirara17233 <Kirara17233@gmail.com>
2021-09-14 18:26:17 +0300

    a

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -25,7 +25,7 @@ main = do
         , modMask            = mod4Mask
         , workspaces         = ["1","2","3","4","5","6","7","8","9"]
         , normalBorderColor  = "#0f111a"
-        , focusedBorderColor = "#8f93a2"
+        , focusedBorderColor = "#ffcc00"
 
         , keys               = myKeys
         , mouseBindings      = myMouseBindings
