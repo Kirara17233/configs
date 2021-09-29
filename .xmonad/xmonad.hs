5d3b5177b700ff7bab42a77e475f831c272baa8b
Kirara17233 <Kirara17233@gmail.com>
2021-09-29 22:22:18 +0300

    a

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -94,7 +94,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- launch rofi
     , ((modm,               xK_p     ), spawn "rofi -show run")
 
-    , ((modm,               xK_x     ), spawn "jetbrains-toolbox; sleep 0.05; wmctrl -r 'Jetbrains toolbox' -e 0,22,$[`xrandr | grep current | awk '{print $10}' | cut -d , -f 1`-704],440,700")
+    , ((modm,               xK_x     ), spawn "jetbrains-toolbox; sleep 0.1; wmctrl -r 'Jetbrains toolbox' -e 0,22,$[`xrandr | grep current | awk '{print $10}' | cut -d , -f 1`-704],440,700")
     , ((modm,               xK_c     ), spawn "code")
     , ((modm,               xK_g     ), spawn "google-chrome-stable")
     , ((modm,               xK_e     ), spawn "Emacs")
